2b124e07845fd286f72b2aff2ca1c4eb26685262
MARIOLEARN <mariodominic23@gmail.com>
2026-08-28 23:19:35 +0700

    Add files via upload

--- task-08.ts
+++ task-08.ts
@@ -1,42 +1,49 @@
 /**
- * The school's Smart Greenhouse system automatically records the air temperature every hour from 08:00 until 15:00. 
- * Instead of storing each temperature in a separate variable, the developer wants to store all temperature readings in a 
- * single collection so they can easily calculate the highest, lowest, and average temperature later.
- * Today, the recorded temperatures are:
+ * A smart home monitors electricity usage every day.
+ * Today's information:
+ * | Information               | Value |
+ * | ------------------------- | ----- |
+ * | Previous Meter            | 25640 |
+ * | Current Meter             | 25892 |
+ * | Electricity Price per kWh | 1650  |
+ * | Solar Panel Installed     | Yes   |
+ * | Energy Saving Mode        | No    |
  * 
-| Time  | Temperature (°C) |
-| ----- | ---------------- |
-| 08:00 | 24.5             |
-| 09:00 | 25.1             |
-| 10:00 | 26.8             |
-| 11:00 | 28.4             |
-| 12:00 | 30.2             |
-| 13:00 | 31.1             |
-| 14:00 | 30.7             |
-| 15:00 | 29.3             |
-
- * Task:
+ * Business Rules
+ * - Electricity usage is calculated from the meter difference.
+ * - Houses with solar panels receive a 20% discount.
+ * - Houses receive an additional 5% discount if Energy Saving Mode is enabled.
+ * - A house qualifies for the Green Energy Program only if:
+ *      - Solar panel is installed
+ *      - Energy consumption is below 300 kWh
+ *      - Energy Saving Mode is enabled
  * 
- * 1. Define a proper type for the temperature information.
- * 2. Store the temperature data in a single collection.
- * 3. Display the temperature data using console.log.
+ * The system must calculate:
+ * - Total energy consumption
+ * - Electricity bill
+ * - Final bill
+ * - Green Energy Program eligibility
  */
 
-type TemperatureReading = {
-  time: string;
-  temperature: number;
-};
+const previousMeter = 25640;
+const currentMeter = 25892;
+const pricePerKwh = 1650;
+const solarPanelInstalled = true;
+const energySavingMode = false;
 
-const temperatureData: TemperatureReading[] = [
-{ time: "08:00", temperature: 24.5 },
-{ time: "09:00", temperature: 25.1 },
-{ time: "10:00", temperature: 26.8 },
-{ time: "11:00", temperature: 28.4 },
-{ time: "12:00", temperature: 30.2 },
-{ time: "13:00", temperature: 31.1 },
-{ time: "14:00", temperature: 30.7 },
-{ time: "15:00", temperature: 29.3 },
-];
+let totalEnergyConsumption = currentMeter - previousMeter;
+let electricityBill = totalEnergyConsumption * pricePerKwh;
+let solarDiscount = solarPanelInstalled ? electricityBill * 0.20 : 0;
+let savingModeDiscount = energySavingMode ? electricityBill * 0.05 : 0;
+let finalBill = electricityBill - solarDiscount - savingModeDiscount;
+let greenEnergyEligible =
+    solarPanelInstalled &&
+    totalEnergyConsumption < 300 &&
+    energySavingMode;
 
-console.log("\n--- Data Suhu dalam Bentuk Array ---");
-console.log(temperatureData);
+
+    
+console.log("Total Energy Consumption:", totalEnergyConsumption, "kWh");
+console.log("Electricity Bill:", electricityBill);
+console.log("Final Bill:", finalBill);
+console.log("Green Energy Program Eligible:", greenEnergyEligible);
